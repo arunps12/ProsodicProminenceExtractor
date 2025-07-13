10a750eefb1836e4c62a420c3254ab40832dc294
arunps12 <mail.arun1289@gmail.com>
2025-07-13 15:22:04 +0200

    Add requirements and package metadata

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prosodicprominence",
+    version="0.1.0",
+    author="Arun Singh",
+    description="A toolkit for extracting word-level prosodic prominence from speech using pitch, energy, and event-based features.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchaudio",
+        "parselmouth",
+        "textgrid",
+        "numpy",
+        "scipy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
