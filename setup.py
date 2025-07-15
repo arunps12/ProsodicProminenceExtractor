1634407d0e6ffadd362537f4602b74cb00de6647
arunps12 <mail.arun1289@gmail.com>
2025-07-15 10:30:24 +0200

    Fix package name in setup.py for correct installation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "torch",
         "torchaudio",
-        "parselmouth",
+        "praat-parselmouth",
         "textgrid",
         "numpy",
         "scipy"
